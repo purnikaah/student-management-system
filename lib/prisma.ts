157e99a8334cd31b611d6411b94e63d216cfaac5
Purnika Lumba <purnikalimbu0@gmail.com>
2026-04-24 11:33:13 +0545

    changes on api endpoints and initial setup

---
+++ lib/prisma.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,20 +4,32 @@ import jwt from "jsonwebtoken";
 import { UserRepository } from "../repositories/user";
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
-export const AuthController = {
-  async login(req: Request, res: Response) {
+
+const login = async (req: Request, res: Response) => {
+  try {
     const { email, password } = req.body;
+
     const user = await UserRepository.findByEmail(email);
+
     if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ error: "Invalid Credintials" });
+      return res.status(401).json({ error: "Invalid Credentials" });
     }
+
     const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
       expiresIn: "1d",
     });
-    res.json({
+
+    return res.json({
       token,
-      message: "Login succesful",
+      message: "Login successful",
       user: { email: user.email },
     });
-  },
+  } catch (error) {
+    console.error("Login Error:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const AuthController = {
+  login,
 };
