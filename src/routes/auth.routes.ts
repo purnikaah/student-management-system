77eb8d826df6ba385bfe21dbfd2966604b29bfca
Purnika Lumba <purnikalimbu0@gmail.com>
2026-04-24 14:04:33 +0545

    minor fixes in router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login } from "../controllers/auth.controller";
+import { AuthController } from "../controllers/login";
+import { login } from "../controllers/login";
 import { authMiddleware } from "../middleware/middleware";
 import { getDashboardRepository } from "../controllers/dashboard";
 const router = Router();
 
-router.post("/login", login);
+router.post("/login", AuthController.login);
 router.get("/summary", authMiddleware, getDashboardRepository);
 export default router;
